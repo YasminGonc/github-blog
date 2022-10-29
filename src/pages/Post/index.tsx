08e59e70bf2248aaaf4a8e3759d325b84dc887d9
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-10-28 23:05:47 -0300

    markdown

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
+import { GitHubContext } from "../../context/GitHubContext";
 import { HeaderCard } from "./components/HeaderCard";
 import { PostConatiner, TextContainer } from "./styles";
+import ReactMarkdown from "react-markdown";
 
 export function Post() {
+    const { issuesContent, postIndex } = useContext(GitHubContext);
     return(
         <PostConatiner>
             <HeaderCard />
             <TextContainer>
-                <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
-                </p>
+                <ReactMarkdown>
+                    {issuesContent[postIndex].body}
+                </ReactMarkdown>
             </TextContainer>
         </PostConatiner>
     )
